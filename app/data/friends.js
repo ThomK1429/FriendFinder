fbd1c1643b8aa8d6aeefb0d1bef329ecbb2b2186
Thomas Keel <sndbytgkv1@verizon.net>
2016-08-27 23:14:30 -0400

    correct data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,19 @@
 var friendArray = 
 
-[{"name":"Lou Ritter","photo":"https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11","scores":["5","5","4","4","3","1","1","1","1","1"]} 
- 
-
+[{"name":"Ahmed","photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg","scores":["5","1","1","1","1","1","1","1","1","1"]},
+ {"name":"Jacob Deming","photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg","scores":["5","5","1","1","1","1","1","1","1","1"]},
+ {"name":"Jeremiah Scanlon","photo":"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460","scores":["5","5","4","1","1","1","1","1","1","1"]},
+ {"name":"Louis T. Delia","photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg","scores":["5","5","4","4","1","1","1","1","1","1"]},
+ {"name":"Geo Washington","photo":"https://www.nps.gov/moru/learn/historyculture/images/Washington-100-x-100.jpg"scores":["5","5","4","4","3","1","1","1","1","1"]},
+ {"name":"Jordan Biason","photo":"https://yt3.ggpht.com/-fZIGYBuyH5M/AAAAAAAAAAI/AAAAAAAAAAA/MmxOlkafsQM/s100-c-k-no-rj-c0xffffff/photo.jpg","scores":["5","5","4","4","3","3","1","1","1","1"]},
+ {"name":"Thomas Edison","photo":"https://upload.wikimedia.org/wikipedia/commons/5/52/Young_Thomas_Edison.jpg","scores":["5","5","4","4","3","3","2","1","1","1"]},
+ {"name":"Mother Teresa","photo":"http://whhspatriotpress.com/wp-content/uploads/2016/03/Mother-Teresa.jpg","scores":["5","5","4","4","3","3","4","2","1","1"]},
+ {"name":"Bill Cullin","photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Bill_Cullen_1954.JPG/220px-Bill_Cullen_1954.JPG","scores":["5","5","4","4","3","3","2","2","0","0"]},
+ {"name":"Henry Ford","photo":"https://media1.britannica.com/eb-media/99/96899-004-C23DA93F.jpg","scores":["5","5","4","4","3","3","2","2","1","0"]},
+ {"name":"Steve Jobs","photo":"https://upload.wikimedia.org/wikipedia/commons/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg","scores":["5","5","4","4","3","3","2","2","1","1"]},
+ {"name":"Bill Gates","photo":"https://pumpkinperson.files.wordpress.com/2016/01/gates.jpg?w=529","scores":["5","5","4","4","3","3","2","2","5","5"]} 
 ]
+
+
  // Note how we export the array. This makes it accessible to other files using require. 
 module.exports = friendArray; 
